ba5fcc2c76dc2333e3ac64e86f4b1601bc031098
deepak <kumarankit1731999@gmail.com>
2026-08-24 15:39:38 +0530

    sa

---
+++ LAB_01/dom.js
@@ -0,0 +1,28 @@
+import { EventEmitter } from 'events';
+
+function createDOMElement() {
+    const emitter = new EventEmitter();
+    return {
+        addEventListener(eventName, callback) {
+            emitter.on(eventName, callback);
+        },
+        removeEventListener(eventName, callback) {
+            emitter.off(eventName, callback);
+        },
+        dispatchEvent(event) {
+            emitter.emit(event.type, event);
+        }
+    };
+}
+
+const button = createDOMElement();
+button.addEventListener('click', (event) => {
+    console.log('Button clicked!', event);
+
+});
+
+button.dispatchEvent({
+    type: 'click',
+    message: 'btn clk'
+});
+
