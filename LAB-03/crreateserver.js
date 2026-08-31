9c2e27ffdaf49c4196d6bfb6de6e5a158e033251
deepak <kumarankit1731999@gmail.com>
2026-08-31 15:32:11 +0530

    3

---
+++ LAB-03/crreateserver.js
@@ -0,0 +1,32 @@
+const http = require('http');
+
+const userdata = {
+    name: 'DEEPAK',
+    age: 30
+};
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
+    if (url === '/msg' && method === 'GET') {
+        res.statusCode = 200;
+        res.end('Welcome to the Home Page');
+
+    }
+    else if (url === '/sys' && method === 'GET') {
+        res.statusCode = 201;
+        res.end('Welcome to the System Page');
+    }
+    else if (url === '/data' && method === 'GET') {
+        res.statusCode = 200;
+        res.end(JSON.stringify(userdata));
+    }
+    else {
+        res.statusCode = 404;
+        res.end('Page not found');
+    }
+});
+
+server.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
